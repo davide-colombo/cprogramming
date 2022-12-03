a74295468dbfdd782676559acf094dc083ef2476
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-03 20:42:07 +0100

    For loop with function call

--- lp.c
+++ lp.c
@@ -7,7 +7,21 @@
 
 #include <stdio.h>
 
+/*
+ * a => 4 bytes
+ * Stack frame == 16 bytes (12 bytes wasted)
+ * return a 4 bytes word => uses `w` register
+ */
+int _lp_inci(int a) {
+	return a+1;
+}
+
 int main(int argc, char** argv) {
+	
+	int a;
+	for (int i = 0; i < 10000; i++) {
+		a = _lp_inci(a);
+	}
 
 	return 0;
 }
--- lp.s
+++ lp.s
@@ -1,18 +1,55 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.build_version macos, 13, 0	sdk_version 13, 0
+	.globl	__lp_inci                       ; -- Begin function _lp_inci
+	.p2align	2
+__lp_inci:                              ; @_lp_inci
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	w0, [sp, #12]
+	ldr	w8, [sp, #12]
+	add	w0, w8, #1
+	add	sp, sp, #16
+	ret
+	.cfi_endproc
+                                        ; -- End function
 	.globl	_main                           ; -- Begin function main
 	.p2align	2
 _main:                                  ; @main
 	.cfi_startproc
 ; %bb.0:
-	sub	sp, sp, #16
-	.cfi_def_cfa_offset 16
-	mov	x8, x0
-	mov	w0, #0
-	str	wzr, [sp, #12]
+	sub	sp, sp, #48
+	stp	x29, x30, [sp, #32]             ; 16-byte Folded Spill
+	add	x29, sp, #32
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	stur	wzr, [x29, #-4]
+	stur	w0, [x29, #-8]
+	str	x1, [sp, #16]
+	str	wzr, [sp, #8]
+	b	LBB1_1
+LBB1_1:                                 ; =>This Inner Loop Header: Depth=1
+	ldr	w8, [sp, #8]
+	mov	w9, #10000
+	subs	w8, w8, w9
+	b.ge	LBB1_4
+	b	LBB1_2
+LBB1_2:                                 ;   in Loop: Header=BB1_1 Depth=1
+	ldr	w0, [sp, #12]
+	bl	__lp_inci
+	str	w0, [sp, #12]
+	b	LBB1_3
+LBB1_3:                                 ;   in Loop: Header=BB1_1 Depth=1
+	ldr	w8, [sp, #8]
+	add	w8, w8, #1
 	str	w8, [sp, #8]
-	str	x1, [sp]
-	add	sp, sp, #16
+	b	LBB1_1
+LBB1_4:
+	mov	w0, #0
+	ldp	x29, x30, [sp, #32]             ; 16-byte Folded Reload
+	add	sp, sp, #48
 	ret
 	.cfi_endproc
                                         ; -- End function
