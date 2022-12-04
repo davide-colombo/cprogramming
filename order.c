e9ba82842fc5b59cf1ee1b566e7bde2a5568e56b
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-04 10:31:59 +0100

    Full initialization of the `order` data structure

--- order.c
+++ order.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdalign.h>
+#include <time.h>
+#include <stdlib.h>
+
+#define NUMBER_OF_ORDERS 1000
+#define NUMBER_OF_BUYERS 5
 
 /*
  * Demonstrating cache locality and how to improve it.
@@ -8,11 +13,34 @@
  * Data structure is 8 bytes aligned.
  */
 struct order {
-	double price;			// 8 bytes
-	bool paid;				// 1 byte
-	const char *buyer[5];	// 5 bytes
-	long buyer_id;			// 8 bytes
+	double price;							// 8 bytes
+	bool paid;								// 1 byte (+ 7 bytes padding)
+	const char *buyer[NUMBER_OF_BUYERS];	// 5 * 8 bytes
+	long buyer_id;							// 8 bytes
 };
+
+/*
+ * Sum the price of all the paid orders corresponding to the buyer_id
+ *
+ * struct order * => 8 bytes
+ *
+ * waste 8 bytes in the stack frame (16 bytes aligned)
+ */
+double _order_sum_paidd(struct order *o) {
+
+}
+
+/*
+ * Sum the price of all the unpaid orders corresponding to the buyer_id
+ *
+ * struct order * => 8 bytes
+ *
+ * waste 8 bytes in the stack frame (16 bytes aligned)
+ */
+double _order_sum_notpaidd(struct order *o) {
+
+}
+
 
 int main(int argc, char** argv) {
 	/*
@@ -24,6 +52,61 @@ int main(int argc, char** argv) {
 	 */
 	printf("sizeof(struct order) = %zu\n", sizeof(struct order));
 	printf("alignof(struct order) = %zu\n", alignof(struct order));
+
+	/*
+	 * Array of buyers name.
+	 *
+	 * This will be stored in the `order` data structure.
+	 *
+	 * There, the 
+	 */
+	const char *buyers[5] = {
+		"buy1", "buy2", "buy3", "buy4", "buy5"
+	};
+
+	/*
+	 * Statically allocate an array of orders
+	 *
+	 * order == &order[0]
+	 *
+	 */
+	struct order orders[NUMBER_OF_ORDERS];
+
+	/*
+	 * Initialize pseudo-random number generator to make the result 
+	 * reproducible.
+	 */
+	srand(time(NULL));
+
+	/*
+	 * Initialize the array of orders
+	 */
+	for(int i = 0; i < NUMBER_OF_ORDERS; ++i) {
+		/*
+		 * Generate a random number between 0 and 1 then scale it between 10.0 
+		 * and 1010.0
+		 */
+		orders[i].price = ((rand() / (double)RAND_MAX) * 1000.0) + 10.0;
+
+		/*
+		 * Paid if `tmp >= 0.5`, otherwise not paid.
+		 *
+		 * Since a new random value is generated this has the benefit of 
+		 * totally decoupling the generation of order price to the paid value.
+		 */
+		double tmp = ( rand() / (double)RAND_MAX);
+		orders[i].paid = ( tmp >= 0.5 ) ? true : false;
+
+		/*
+		 * Initialize buyer names
+		 */
+		for(int b = 0; b < NUMBER_OF_BUYERS; ++b) {
+			orders[i].buyer[b] = buyers[b];
+		}
+		
+		int tmp_id = (int)(( rand() / (double)RAND_MAX ) * 5.0);
+		orders[i].buyer_id = tmp_id;
+	}
 
 	return 0;
 }
--- order.s
+++ order.s
@@ -1,35 +1,160 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.build_version macos, 13, 0	sdk_version 13, 0
-	.globl	_main                           ; -- Begin function main
+	.globl	__order_sum_paidd               ; -- Begin function _order_sum_paidd
+	.p2align	2
+__order_sum_paidd:                      ; @_order_sum_paidd
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	x0, [sp]
+	ldr	d0, [sp, #8]
+	add	sp, sp, #16
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.globl	__order_sum_notpaidd            ; -- Begin function _order_sum_notpaidd
+	.p2align	2
+__order_sum_notpaidd:                   ; @_order_sum_notpaidd
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	x0, [sp]
+	ldr	d0, [sp, #8]
+	add	sp, sp, #16
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__literal8,8byte_literals
+	.p2align	3                               ; -- Begin function main
+lCPI2_0:
+	.quad	0x408f400000000000              ; double 1000
+	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_main
 	.p2align	2
 _main:                                  ; @main
 	.cfi_startproc
 ; %bb.0:
-	sub	sp, sp, #48
-	stp	x29, x30, [sp, #32]             ; 16-byte Folded Spill
-	add	x29, sp, #32
+	stp	x28, x27, [sp, #-32]!           ; 16-byte Folded Spill
+	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
+	add	x29, sp, #16
+	mov	w9, #64048
+	adrp	x16, ___chkstk_darwin@GOTPAGE
+	ldr	x16, [x16, ___chkstk_darwin@GOTPAGEOFF]
+	blr	x16
+	sub	sp, sp, #15, lsl #12            ; =61440
+	sub	sp, sp, #2608
 	.cfi_def_cfa w29, 16
 	.cfi_offset w30, -8
 	.cfi_offset w29, -16
-	mov	w8, #0
-	str	w8, [sp, #12]                   ; 4-byte Folded Spill
-	stur	wzr, [x29, #-4]
-	stur	w0, [x29, #-8]
-	str	x1, [sp, #16]
+	.cfi_offset w27, -24
+	.cfi_offset w28, -32
+	adrp	x8, ___stack_chk_guard@GOTPAGE
+	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
+	ldr	x8, [x8]
+	stur	x8, [x29, #-24]
+	str	wzr, [sp, #36]
+	str	w0, [sp, #32]
+	str	x1, [sp, #24]
 	mov	x9, sp
 	mov	x8, #64
 	str	x8, [x9]
 	adrp	x0, l_.str@PAGE
 	add	x0, x0, l_.str@PAGEOFF
 	bl	_printf
-	ldr	w0, [sp, #12]                   ; 4-byte Folded Reload
-	ldp	x29, x30, [sp, #32]             ; 16-byte Folded Reload
-	add	sp, sp, #48
+	mov	x9, sp
+	mov	x8, #8
+	str	x8, [x9]
+	adrp	x0, l_.str.1@PAGE
+	add	x0, x0, l_.str.1@PAGEOFF
+	bl	_printf
+	mov	x0, #0
+	bl	_time
+                                        ; kill: def $w0 killed $w0 killed $x0
+	bl	_srand
+	str	wzr, [sp, #20]
+	b	LBB2_1
+LBB2_1:                                 ; =>This Inner Loop Header: Depth=1
+	ldr	w8, [sp, #20]
+	subs	w8, w8, #1000
+	b.ge	LBB2_4
+	b	LBB2_2
+LBB2_2:                                 ;   in Loop: Header=BB2_1 Depth=1
+	bl	_rand
+	mov	w8, #2147483647
+	sdiv	w8, w0, w8
+	scvtf	d0, w8
+	adrp	x8, lCPI2_0@PAGE
+	ldr	d1, [x8, lCPI2_0@PAGEOFF]
+	fmul	d0, d0, d1
+	fmov	d1, #10.00000000
+	fadd	d0, d0, d1
+	ldrsw	x8, [sp, #20]
+	lsl	x9, x8, #6
+	add	x8, sp, #40
+	str	d0, [x8, x9]
+	b	LBB2_3
+LBB2_3:                                 ;   in Loop: Header=BB2_1 Depth=1
+	ldr	w8, [sp, #20]
+	add	w8, w8, #1
+	str	w8, [sp, #20]
+	b	LBB2_1
+LBB2_4:
+	str	wzr, [sp, #16]
+	b	LBB2_5
+LBB2_5:                                 ; =>This Inner Loop Header: Depth=1
+	ldr	w8, [sp, #16]
+	subs	w8, w8, #1000
+	b.ge	LBB2_8
+	b	LBB2_6
+LBB2_6:                                 ;   in Loop: Header=BB2_5 Depth=1
+	ldr	w8, [sp, #16]
+                                        ; implicit-def: $x9
+	mov	x9, x8
+	ldrsw	x8, [sp, #16]
+	lsl	x10, x8, #6
+	add	x8, sp, #40
+	ldr	d0, [x8, x10]
+	mov	x8, sp
+	str	x9, [x8]
+	str	d0, [x8, #8]
+	adrp	x0, l_.str.2@PAGE
+	add	x0, x0, l_.str.2@PAGEOFF
+	bl	_printf
+	b	LBB2_7
+LBB2_7:                                 ;   in Loop: Header=BB2_5 Depth=1
+	ldr	w8, [sp, #16]
+	add	w8, w8, #1
+	str	w8, [sp, #16]
+	b	LBB2_5
+LBB2_8:
+	ldur	x9, [x29, #-24]
+	adrp	x8, ___stack_chk_guard@GOTPAGE
+	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
+	ldr	x8, [x8]
+	subs	x8, x8, x9
+	b.eq	LBB2_10
+	b	LBB2_9
+LBB2_9:
+	bl	___stack_chk_fail
+LBB2_10:
+	mov	w0, #0
+	add	sp, sp, #15, lsl #12            ; =61440
+	add	sp, sp, #2608
+	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
+	ldp	x28, x27, [sp], #32             ; 16-byte Folded Reload
 	ret
 	.cfi_endproc
                                         ; -- End function
 	.section	__TEXT,__cstring,cstring_literals
 l_.str:                                 ; @.str
 	.asciz	"sizeof(struct order) = %zu\n"
+
+l_.str.1:                               ; @.str.1
+	.asciz	"alignof(struct order) = %zu\n"
+
+l_.str.2:                               ; @.str.2
+	.asciz	"orders[%d].price = %.2f\n"
 
 .subsections_via_symbols
