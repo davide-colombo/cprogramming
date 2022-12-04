fd1f2c9e43a33431b6b42306fa0383343034ccb5
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-04 14:59:28 +0100

    Split order and buyer

--- order.c
+++ order.c
@@ -6,49 +6,124 @@
 
 #define NUMBER_OF_ORDERS 1000
 #define NUMBER_OF_BUYERS 5
+#define NUMBER_OF_ORDERS_PER_BUYER	(NUMBER_OF_ORDERS / NUMBER_OF_BUYERS)
 
 /*
- * Demonstrating cache locality and how to improve it.
+ * Cache line size
+ */
+#define CLS 128
+
+/*
+ * The goal of this program is to compute the total amount of paid and unpaid 
+ * orders for each buyer.
  *
- * Data structure is 8 bytes aligned.
+ * Each order is associated to a buyer identifier and has a price.
+ *
+ * The first implementation (inefficient) of this program relies on a data 
+ * structure that packed together buyer identifier, the price of the order and 
+ * a boolean flag that is true if the order is paid.
+ *
+ * This is inefficient for different reasons.
+ *
+ * The first is the encoding of the boolean flag into the data structure.
+ *
+ * This is not efficient since it moves the `conditional branch` lower in the 
+ * loop that computes the sum of the price.
+ *
+ * This means that at every loop iteration the program needs to test a 
+ * condition and branch.
+ *
+ * Also consider that the flag is stored as a 1 byte value but only 1 bit 
+ * could be used to encode the value true or false.
+ *
+ * This is already a `smell` of an unoptimal approach.
+ *
+ * The second inefficiency came from the fact that the buyer identifier is 
+ * stored together with the order.
+ *
+ * The total sum of the orders needs the price, not the buyer's identifier.
+ *
+ * This is inefficient because it `pollutes` the cache lines with UNNECESSARY 
+ * DATA.
+ *
+ * Even worst when the data structure contains the boolean flag.
+ *
+ * A better approach should deal with both SPATIAL & TEMPORAL LOCALITY of the 
+ * data.
+ *
+ * To understand how to reorganize the data structure it's important to start 
+ * from the question:
+ *
+ *		`what has to be computed by this program?`
+ *
+ * To maximize SPATIAL LOCALITY the goal is to pack together data that will be 
+ * processed together sequentially.
+ *
+ * To maximize TEMPORAL LOCALITY the goal is to define a small set of 
+ * instructions that can be executed many times sequentially.
+ *
+ * Since the goal of the program is to compute the total amount of paid and 
+ * unpaid orders it is ideal to split the two into different data structures 
+ * and associate each of them to the buyer's identifier.
+ *
+ * This approach ensures both SPATIAL LOCALITY by removing the UNNECESSARY 
+ * buyer identifier from the data structure and avoid to polluting the cache 
+ * line.
+ *
+ * Also, it provides a better TEMPORAL LOCALITY since the computation can be 
+ * expressed in terms of small number of instructions repeated many times.
+ *
+ * This solution also avoids to test whether the order is paid or not at every 
+ * loop iteration.
+ *
+ * To recap:
+ *
+ * 1. pushed out of the loop the `conditional branch` by splitting orders into 
+ *    two data structures;
+ * 2. move at a higher level of abstraction the KEYWORD used to identify a 
+ *    single item of interest (in this example is the buyer identifier).
+ *
+ */
+
+// ===========================================================================
+
+/*
+ * The elements must be aligned on a MEMORY ADDRESS that is a multiple of the 
+ * CACHE LINE SIZE.
+ *
+ * Items of this data structure are loaded into the CACHE for computations.
+ *
+ * Data structure aligned on 8 bytes boundary with a total size of 8 bytes.
+ *
+ * With a CACHE LINE SIZE of 128 bytes it's possible to contain 16 orders in 
+ * each cache line.
+ *
  */
 struct order {
-	double price;							// 8 bytes
-	bool paid;								// 1 byte (+ 7 bytes padding)
-	const char *buyer[NUMBER_OF_BUYERS];	// 5 * 8 bytes
-	long buyer_id;							// 8 bytes
+	double price;		// 8 bytes
+} __attribute__((aligned (CLS) ));
+
+/*
+ * The buyer is not necessary to make computations on the orders.
+ */
+struct buyer_orders {
+	long buyer_id;						// 8 bytes
+	struct order *paid_orders;			// 8 bytes
+	size_t number_of_paid_orders;		// 8 bytes
+	struct order *unpaid_orders;		// 8 bytes
+	size_t number_of_unpaid_orders;		// 8 bytes
 };
 
 /*
- * Sum the price of all the paid orders corresponding to the buyer_id
- *
- * struct order * => 8 bytes
- *
- * waste 8 bytes in the stack frame (16 bytes aligned)
+ * stack frame 16 bytes
+ * 8 bytes allocated for the first argument
+ * 8 bytes allocated for the second argument
+ * 8 bytes allocated for the local variable
  */
-double _order_sum_paidd(struct order *o, long buyer_id) {
+double _order_sum_priced(struct order *orders, size_t number_of_orders) {
 	double sum = 0;
-	for(int i = 0; i < NUMBER_OF_ORDERS; ++i) {
-		if(o[i].buyer_id == buyer_id && o[i].paid) {
-			sum += o[i].price;
-		}
-	}
-	return sum;
-}
-
-/*
- * Sum the price of all the unpaid orders corresponding to the buyer_id
- *
- * struct order * => 8 bytes
- *
- * waste 8 bytes in the stack frame (16 bytes aligned)
- */
-double _order_sum_notpaidd(struct order *o, long buyer_id) {
-	double sum = 0;
-	for(int i = 0; i < NUMBER_OF_ORDERS; ++i) {
-		if(o[i].buyer_id == buyer_id && !o[i].paid) {
-			sum += o[i].price;
-		}
+	for(size_t i = 0; i < number_of_orders; ++i) {
+		sum += orders[i].price;
 	}
 	return sum;
 }
@@ -62,91 +137,91 @@ int main(int argc, char** argv) {
 	 *   largest data type in the `struct` (8 bytes).
 	 *
 	 */
-	printf("sizeof(struct order) = %zu\n", sizeof(struct order));
-	printf("alignof(struct order) = %zu\n", alignof(struct order));
+	printf("sizeof(struct order) = %zu\n", sizeof(struct buyer_orders));
+	printf("alignof(struct order) = %zu\n", alignof(struct buyer_orders));
 
-	/*
-	 * Array of buyers name.
-	 *
-	 * This will be stored in the `order` data structure.
-	 *
-	 * There, the 
-	 */
-	const char *buyers[5] = {
-		"buy1", "buy2", "buy3", "buy4", "buy5"
-	};
-
-	/*
-	 * Statically allocate an array of orders
-	 *
-	 * order == &order[0]
-	 *
-	 */
-	struct order orders[NUMBER_OF_ORDERS];
-
-	/*
-	 * Initialize pseudo-random number generator to make the result 
-	 * reproducible.
-	 */
-	srand(time(NULL));
-	
-	/*
-	 * Record the start of the initialization
-	 */
-	clock_t start_clock, end_clock;
-	start_clock = clock();
-	/*
-	 * Initialize the array of orders
-	 */
-	for(int i = 0; i < NUMBER_OF_ORDERS; ++i) {
-		/*
-		 * Generate a random number between 0 and 1 then scale it between 10.0 
-		 * and 1010.0
-		 */
-		orders[i].price = ((rand() / (double)RAND_MAX) * 1000.0) + 10.0;
-
-		/*
-		 * Paid if `tmp >= 0.5`, otherwise not paid.
-		 *
-		 * Since a new random value is generated this has the benefit of 
-		 * totally decoupling the generation of order price to the paid value.
-		 */
-		double tmp = ( rand() / (double)RAND_MAX);
-		orders[i].paid = ( tmp >= 0.5 ) ? true : false;
-
-		/*
-		 * Initialize buyer names
-		 */
-		for(int b = 0; b < NUMBER_OF_BUYERS; ++b) {
-			orders[i].buyer[b] = buyers[b];
-		}
-		
-		long tmp_id = (long)(( rand() / (double)RAND_MAX ) * NUMBER_OF_BUYERS);
-		orders[i].buyer_id = tmp_id;
-	}
-
-	end_clock = clock();
-	unsigned long elapsed_clock = (end_clock - start_clock);
-	printf("CLOCKS_PER_SEC = %lu\n", (unsigned long)CLOCKS_PER_SEC);
-	printf("elapsed_clock = %lu\n", elapsed_clock);
-
-	/*
-	 * Measure how much time it takes to compute the total amount of paid and 
-	 * not-paid orders.
-	 */
-	start_clock = clock();
-
-	/*
-	 * Sum paid orders for each buyer ID
-	 */
-	for(long j = 0; j < NUMBER_OF_BUYERS; ++j) {
-		printf("[`buyer_id` = %ld]\ttotal paid = %.4f\n", j, _order_sum_paidd(orders, j));
-		printf("[`buyer_id` = %ld]\ttotal NOT paid = %.4f\n", j, _order_sum_notpaidd(orders, j));
-	}
-
-	end_clock = clock();
-	elapsed_clock = (end_clock - start_clock);
-	printf("elapsed_clock = %lu\n", elapsed_clock);
+//	/*
+//	 * Array of buyers name.
+//	 *
+//	 * This will be stored in the `order` data structure.
+//	 *
+//	 * There, the 
+//	 */
+//	const char *buyers[5] = {
+//		"buy1", "buy2", "buy3", "buy4", "buy5"
+//	};
+//
+//	/*
+//	 * Statically allocate an array of orders
+//	 *
+//	 * order == &order[0]
+//	 *
+//	 */
+//	struct order orders[NUMBER_OF_ORDERS];
+//
+//	/*
+//	 * Initialize pseudo-random number generator to make the result 
+//	 * reproducible.
+//	 */
+//	srand(time(NULL));
+//	
+//	/*
+//	 * Record the start of the initialization
+//	 */
+//	clock_t start_clock, end_clock;
+//	start_clock = clock();
+//	/*
+//	 * Initialize the array of orders
+//	 */
+//	for(int i = 0; i < NUMBER_OF_ORDERS; ++i) {
+//		/*
+//		 * Generate a random number between 0 and 1 then scale it between 10.0 
+//		 * and 1010.0
+//		 */
+//		orders[i].price = ((rand() / (double)RAND_MAX) * 1000.0) + 10.0;
+//
+//		/*
+//		 * Paid if `tmp >= 0.5`, otherwise not paid.
+//		 *
+//		 * Since a new random value is generated this has the benefit of 
+//		 * totally decoupling the generation of order price to the paid value.
+//		 */
+//		double tmp = ( rand() / (double)RAND_MAX);
+//		orders[i].paid = ( tmp >= 0.5 ) ? true : false;
+//
+//		/*
+//		 * Initialize buyer names
+//		 */
+//		for(int b = 0; b < NUMBER_OF_BUYERS; ++b) {
+//			orders[i].buyer[b] = buyers[b];
+//		}
+//		
+//		long tmp_id = (long)(( rand() / (double)RAND_MAX ) * NUMBER_OF_BUYERS);
+//		orders[i].buyer_id = tmp_id;
+//	}
+//
+//	end_clock = clock();
+//	unsigned long elapsed_clock = (end_clock - start_clock);
+//	printf("CLOCKS_PER_SEC = %lu\n", (unsigned long)CLOCKS_PER_SEC);
+//	printf("elapsed_clock = %lu\n", elapsed_clock);
+//
+//	/*
+//	 * Measure how much time it takes to compute the total amount of paid and 
+//	 * not-paid orders.
+//	 */
+//	start_clock = clock();
+//
+//	/*
+//	 * Sum paid orders for each buyer ID
+//	 */
+//	for(long j = 0; j < NUMBER_OF_BUYERS; ++j) {
+//		printf("[`buyer_id` = %ld]\ttotal paid = %.4f\n", j, _order_sum_paidd(orders, j));
+//		printf("[`buyer_id` = %ld]\ttotal NOT paid = %.4f\n", j, _order_sum_notpaidd(orders, j));
+//	}
+//
+//	end_clock = clock();
+//	elapsed_clock = (end_clock - start_clock);
+//	printf("elapsed_clock = %lu\n", elapsed_clock);
 
 	return 0;
 }
--- order.s
+++ order.s
@@ -1,64 +1,62 @@
 	.section	__TEXT,__text,regular,pure_instructions
 	.build_version macos, 13, 0	sdk_version 13, 0
-	.globl	__order_sum_paidd               ; -- Begin function _order_sum_paidd
+	.globl	__order_sum_priced              ; -- Begin function _order_sum_priced
 	.p2align	2
-__order_sum_paidd:                      ; @_order_sum_paidd
+__order_sum_priced:                     ; @_order_sum_priced
 	.cfi_startproc
 ; %bb.0:
-	sub	sp, sp, #16
-	.cfi_def_cfa_offset 16
-	str	x0, [sp]
+	sub	sp, sp, #32
+	.cfi_def_cfa_offset 32
+	str	x0, [sp, #24]
+	str	x1, [sp, #16]
+	movi	d0, #0000000000000000
+	str	d0, [sp, #8]
+	str	xzr, [sp]
+	b	LBB0_1
+LBB0_1:                                 ; =>This Inner Loop Header: Depth=1
+	ldr	x8, [sp]
+	ldr	x9, [sp, #16]
+	subs	x8, x8, x9
+	b.hs	LBB0_4
+	b	LBB0_2
+LBB0_2:                                 ;   in Loop: Header=BB0_1 Depth=1
+	ldr	x8, [sp, #24]
+	ldr	x9, [sp]
+	lsl	x9, x9, #7
+	ldr	d1, [x8, x9]
 	ldr	d0, [sp, #8]
-	add	sp, sp, #16
+	fadd	d0, d0, d1
+	str	d0, [sp, #8]
+	b	LBB0_3
+LBB0_3:                                 ;   in Loop: Header=BB0_1 Depth=1
+	ldr	x8, [sp]
+	add	x8, x8, #1
+	str	x8, [sp]
+	b	LBB0_1
+LBB0_4:
+	ldr	d0, [sp, #8]
+	add	sp, sp, #32
 	ret
 	.cfi_endproc
                                         ; -- End function
-	.globl	__order_sum_notpaidd            ; -- Begin function _order_sum_notpaidd
-	.p2align	2
-__order_sum_notpaidd:                   ; @_order_sum_notpaidd
-	.cfi_startproc
-; %bb.0:
-	sub	sp, sp, #16
-	.cfi_def_cfa_offset 16
-	str	x0, [sp]
-	ldr	d0, [sp, #8]
-	add	sp, sp, #16
-	ret
-	.cfi_endproc
-                                        ; -- End function
-	.section	__TEXT,__literal8,8byte_literals
-	.p2align	3                               ; -- Begin function main
-lCPI2_0:
-	.quad	0x408f400000000000              ; double 1000
-	.section	__TEXT,__text,regular,pure_instructions
-	.globl	_main
+	.globl	_main                           ; -- Begin function main
 	.p2align	2
 _main:                                  ; @main
 	.cfi_startproc
 ; %bb.0:
-	stp	x28, x27, [sp, #-32]!           ; 16-byte Folded Spill
-	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
-	add	x29, sp, #16
-	mov	w9, #64048
-	adrp	x16, ___chkstk_darwin@GOTPAGE
-	ldr	x16, [x16, ___chkstk_darwin@GOTPAGEOFF]
-	blr	x16
-	sub	sp, sp, #15, lsl #12            ; =61440
-	sub	sp, sp, #2608
+	sub	sp, sp, #48
+	stp	x29, x30, [sp, #32]             ; 16-byte Folded Spill
+	add	x29, sp, #32
 	.cfi_def_cfa w29, 16
 	.cfi_offset w30, -8
 	.cfi_offset w29, -16
-	.cfi_offset w27, -24
-	.cfi_offset w28, -32
-	adrp	x8, ___stack_chk_guard@GOTPAGE
-	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
-	ldr	x8, [x8]
-	stur	x8, [x29, #-24]
-	str	wzr, [sp, #36]
-	str	w0, [sp, #32]
-	str	x1, [sp, #24]
+	mov	w8, #0
+	str	w8, [sp, #12]                   ; 4-byte Folded Spill
+	stur	wzr, [x29, #-4]
+	stur	w0, [x29, #-8]
+	str	x1, [sp, #16]
 	mov	x9, sp
-	mov	x8, #64
+	mov	x8, #40
 	str	x8, [x9]
 	adrp	x0, l_.str@PAGE
 	add	x0, x0, l_.str@PAGEOFF
@@ -69,81 +67,9 @@ _main:                                  ; @main
 	adrp	x0, l_.str.1@PAGE
 	add	x0, x0, l_.str.1@PAGEOFF
 	bl	_printf
-	mov	x0, #0
-	bl	_time
-                                        ; kill: def $w0 killed $w0 killed $x0
-	bl	_srand
-	str	wzr, [sp, #20]
-	b	LBB2_1
-LBB2_1:                                 ; =>This Inner Loop Header: Depth=1
-	ldr	w8, [sp, #20]
-	subs	w8, w8, #1000
-	b.ge	LBB2_4
-	b	LBB2_2
-LBB2_2:                                 ;   in Loop: Header=BB2_1 Depth=1
-	bl	_rand
-	mov	w8, #2147483647
-	sdiv	w8, w0, w8
-	scvtf	d0, w8
-	adrp	x8, lCPI2_0@PAGE
-	ldr	d1, [x8, lCPI2_0@PAGEOFF]
-	fmul	d0, d0, d1
-	fmov	d1, #10.00000000
-	fadd	d0, d0, d1
-	ldrsw	x8, [sp, #20]
-	lsl	x9, x8, #6
-	add	x8, sp, #40
-	str	d0, [x8, x9]
-	b	LBB2_3
-LBB2_3:                                 ;   in Loop: Header=BB2_1 Depth=1
-	ldr	w8, [sp, #20]
-	add	w8, w8, #1
-	str	w8, [sp, #20]
-	b	LBB2_1
-LBB2_4:
-	str	wzr, [sp, #16]
-	b	LBB2_5
-LBB2_5:                                 ; =>This Inner Loop Header: Depth=1
-	ldr	w8, [sp, #16]
-	subs	w8, w8, #1000
-	b.ge	LBB2_8
-	b	LBB2_6
-LBB2_6:                                 ;   in Loop: Header=BB2_5 Depth=1
-	ldr	w8, [sp, #16]
-                                        ; implicit-def: $x9
-	mov	x9, x8
-	ldrsw	x8, [sp, #16]
-	lsl	x10, x8, #6
-	add	x8, sp, #40
-	ldr	d0, [x8, x10]
-	mov	x8, sp
-	str	x9, [x8]
-	str	d0, [x8, #8]
-	adrp	x0, l_.str.2@PAGE
-	add	x0, x0, l_.str.2@PAGEOFF
-	bl	_printf
-	b	LBB2_7
-LBB2_7:                                 ;   in Loop: Header=BB2_5 Depth=1
-	ldr	w8, [sp, #16]
-	add	w8, w8, #1
-	str	w8, [sp, #16]
-	b	LBB2_5
-LBB2_8:
-	ldur	x9, [x29, #-24]
-	adrp	x8, ___stack_chk_guard@GOTPAGE
-	ldr	x8, [x8, ___stack_chk_guard@GOTPAGEOFF]
-	ldr	x8, [x8]
-	subs	x8, x8, x9
-	b.eq	LBB2_10
-	b	LBB2_9
-LBB2_9:
-	bl	___stack_chk_fail
-LBB2_10:
-	mov	w0, #0
-	add	sp, sp, #15, lsl #12            ; =61440
-	add	sp, sp, #2608
-	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
-	ldp	x28, x27, [sp], #32             ; 16-byte Folded Reload
+	ldr	w0, [sp, #12]                   ; 4-byte Folded Reload
+	ldp	x29, x30, [sp, #32]             ; 16-byte Folded Reload
+	add	sp, sp, #48
 	ret
 	.cfi_endproc
                                         ; -- End function
@@ -153,8 +79,5 @@ l_.str:                                 ; @.str
 
 l_.str.1:                               ; @.str.1
 	.asciz	"alignof(struct order) = %zu\n"
-
-l_.str.2:                               ; @.str.2
-	.asciz	"orders[%d].price = %.2f\n"
 
 .subsections_via_symbols
