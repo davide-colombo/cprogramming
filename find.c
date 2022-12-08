21fe1f9c724761b6b0b8d9c4a6ee3b3694b47c1f
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-08 12:48:39 +0100

    Using pointer or array index does not affect asm

--- find.c
+++ find.c
@@ -16,14 +16,14 @@ uint32_t ia[N_ITEMS];
 
 int main(int argc, char **argv) {
 	for(int i = 0; i < N_ITEMS; i++){
-		ia[i] = i;
+		*(ia+i) = i;
 	}
 
 	uint32_t *ptr = ia;
 	uint32_t val = 74;
 	/*
 	 * If two values are equal, the XOR returns 0.
-	 * */
+	 */
 	while((*ptr ^ val) != 0){
 		++ptr;
 	}
