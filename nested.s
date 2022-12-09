3592d68dd3135c0d60c4e0f63458372ebffa5bea
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-09 16:34:09 +0100

    Do-while loop based on pointers: version 2

--- nested.s
+++ nested.s
@@ -7,94 +7,72 @@ __loop_rowise:                          ; @_loop_rowise
 ; %bb.0:
 	sub	sp, sp, #32
 	.cfi_def_cfa_offset 32
-	str	wzr, [sp, #20]
+	adrp	x10, _ia@GOTPAGE
+	ldr	x10, [x10, _ia@GOTPAGEOFF]
+	add	x9, x10, #16, lsl #12           ; =65536
+	add	x8, x10, #512
+	str	x10, [sp, #24]
+	str	x9, [sp, #16]
+	str	x8, [sp, #8]
 	b	LBB0_1
 LBB0_1:                                 ; =>This Loop Header: Depth=1
-                                        ;     Child Loop BB0_3 Depth 2
-                                        ;       Child Loop BB0_5 Depth 3
-                                        ;         Child Loop BB0_7 Depth 4
-	ldr	w8, [sp, #20]
-	subs	w8, w8, #128
-	b.ge	LBB0_16
+                                        ;     Child Loop BB0_2 Depth 2
 	b	LBB0_2
-LBB0_2:                                 ;   in Loop: Header=BB0_1 Depth=1
-	str	wzr, [sp, #16]
+LBB0_2:                                 ;   Parent Loop BB0_1 Depth=1
+                                        ; =>  This Inner Loop Header: Depth=2
+	ldr	x8, [sp, #24]
+	ldr	x9, [sp, #8]
+	subs	x8, x8, x9
+	b.hs	LBB0_4
 	b	LBB0_3
-LBB0_3:                                 ;   Parent Loop BB0_1 Depth=1
-                                        ; =>  This Loop Header: Depth=2
-                                        ;       Child Loop BB0_5 Depth 3
-                                        ;         Child Loop BB0_7 Depth 4
-	ldr	w8, [sp, #16]
-	subs	w8, w8, #128
-	b.ge	LBB0_14
-	b	LBB0_4
-LBB0_4:                                 ;   in Loop: Header=BB0_3 Depth=2
-	str	wzr, [sp, #12]
-	ldrsw	x9, [sp, #20]
-	adrp	x8, _ia@GOTPAGE
-	ldr	x8, [x8, _ia@GOTPAGEOFF]
-	add	x8, x8, x9, lsl #9
-	ldrsw	x9, [sp, #16]
-	add	x8, x8, x9, lsl #2
-	str	x8, [sp, #24]
-	b	LBB0_5
-LBB0_5:                                 ;   Parent Loop BB0_1 Depth=1
-                                        ;     Parent Loop BB0_3 Depth=2
-                                        ; =>    This Loop Header: Depth=3
-                                        ;         Child Loop BB0_7 Depth 4
-	ldrsw	x8, [sp, #12]
-	subs	x8, x8, #32
-	b.hs	LBB0_12
-	b	LBB0_6
-LBB0_6:                                 ;   in Loop: Header=BB0_5 Depth=3
-	str	wzr, [sp, #8]
-	b	LBB0_7
-LBB0_7:                                 ;   Parent Loop BB0_1 Depth=1
-                                        ;     Parent Loop BB0_3 Depth=2
-                                        ;       Parent Loop BB0_5 Depth=3
-                                        ; =>      This Inner Loop Header: Depth=4
-	ldrsw	x8, [sp, #8]
-	subs	x8, x8, #32
-	b.hs	LBB0_10
-	b	LBB0_8
-LBB0_8:                                 ;   in Loop: Header=BB0_7 Depth=4
+LBB0_3:                                 ;   in Loop: Header=BB0_2 Depth=2
 	ldr	x9, [sp, #24]
-	ldrsw	x10, [sp, #8]
+	add	x8, x9, #4
+	str	x8, [sp, #24]
 	mov	w8, #1
-	str	w8, [x9, x10, lsl #2]
-	b	LBB0_9
-LBB0_9:                                 ;   in Loop: Header=BB0_7 Depth=4
-	ldr	w8, [sp, #8]
-	add	w8, w8, #1
-	str	w8, [sp, #8]
-	b	LBB0_7
-LBB0_10:                                ;   in Loop: Header=BB0_5 Depth=3
-	b	LBB0_11
-LBB0_11:                                ;   in Loop: Header=BB0_5 Depth=3
-	ldr	w8, [sp, #12]
-	add	w8, w8, #1
-	str	w8, [sp, #12]
+	str	w8, [sp, #4]                    ; 4-byte Folded Spill
+	str	w8, [x9]
+	ldr	x9, [sp, #24]
+	add	x10, x9, #4
+	str	x10, [sp, #24]
+	str	w8, [x9]
+	ldr	x9, [sp, #24]
+	add	x10, x9, #4
+	str	x10, [sp, #24]
+	str	w8, [x9]
+	ldr	x9, [sp, #24]
+	add	x10, x9, #4
+	str	x10, [sp, #24]
+	str	w8, [x9]
+	ldr	x9, [sp, #24]
+	add	x10, x9, #4
+	str	x10, [sp, #24]
+	str	w8, [x9]
+	ldr	x9, [sp, #24]
+	add	x10, x9, #4
+	str	x10, [sp, #24]
+	str	w8, [x9]
+	ldr	x9, [sp, #24]
+	add	x10, x9, #4
+	str	x10, [sp, #24]
+	str	w8, [x9]
+	ldr	x9, [sp, #24]
+	add	x10, x9, #4
+	str	x10, [sp, #24]
+	str	w8, [x9]
+	b	LBB0_2
+LBB0_4:                                 ;   in Loop: Header=BB0_1 Depth=1
 	ldr	x8, [sp, #24]
 	add	x8, x8, #512
-	str	x8, [sp, #24]
+	str	x8, [sp, #8]
 	b	LBB0_5
-LBB0_12:                                ;   in Loop: Header=BB0_3 Depth=2
-	b	LBB0_13
-LBB0_13:                                ;   in Loop: Header=BB0_3 Depth=2
-	ldrsw	x8, [sp, #16]
-	add	x8, x8, #32
-                                        ; kill: def $w8 killed $w8 killed $x8
-	str	w8, [sp, #16]
-	b	LBB0_3
-LBB0_14:                                ;   in Loop: Header=BB0_1 Depth=1
-	b	LBB0_15
-LBB0_15:                                ;   in Loop: Header=BB0_1 Depth=1
-	ldrsw	x8, [sp, #20]
-	add	x8, x8, #32
-                                        ; kill: def $w8 killed $w8 killed $x8
-	str	w8, [sp, #20]
-	b	LBB0_1
-LBB0_16:
+LBB0_5:                                 ;   in Loop: Header=BB0_1 Depth=1
+	ldr	x8, [sp, #24]
+	ldr	x9, [sp, #16]
+	subs	x8, x8, x9
+	b.ne	LBB0_1
+	b	LBB0_6
+LBB0_6:
 	add	sp, sp, #32
 	ret
 	.cfi_endproc
