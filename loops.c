5517d65c20edc0ae743a29b31711cb4177b53e82
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-15 17:18:05 +0100

    Benchmark (baseline): sloppy for!
    In this version I measured the time the for loop it takes to compute the
    sum of all the values and it is a lot.

--- loops.c
+++ loops.c
@@ -4,6 +4,7 @@
  */
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
 
 #define N 100000000
 
@@ -13,3 +14,13 @@ void for1_inc(){
 		sum = sum + 1;
 	}
 }
+
+int main(int argc, char *argv[]){
+	clock_t start, end;
+	start = clock();
+	for1_inc();
+	end = clock();
+	printf("Elapsed: %.20lf\n", (end - start) / (double)CLOCKS_PER_SEC);
+	return 0;
+}
+
--- loops.s
+++ loops.s
@@ -32,4 +32,52 @@ LBB0_4:
 	ret
 	.cfi_endproc
                                         ; -- End function
+	.section	__TEXT,__literal8,8byte_literals
+	.p2align	3                               ; -- Begin function main
+lCPI1_0:
+	.quad	0x412e848000000000              ; double 1.0E+6
+	.section	__TEXT,__text,regular,pure_instructions
+	.globl	_main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	sub	sp, sp, #64
+	stp	x29, x30, [sp, #48]             ; 16-byte Folded Spill
+	add	x29, sp, #48
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	mov	w8, #0
+	str	w8, [sp, #12]                   ; 4-byte Folded Spill
+	stur	wzr, [x29, #-4]
+	stur	w0, [x29, #-8]
+	stur	x1, [x29, #-16]
+	bl	_clock
+	str	x0, [sp, #24]
+	bl	_for1_inc
+	bl	_clock
+	str	x0, [sp, #16]
+	ldr	x8, [sp, #16]
+	ldr	x9, [sp, #24]
+	subs	x8, x8, x9
+	ucvtf	d0, x8
+	adrp	x8, lCPI1_0@PAGE
+	ldr	d1, [x8, lCPI1_0@PAGEOFF]
+	fdiv	d0, d0, d1
+	mov	x8, sp
+	str	d0, [x8]
+	adrp	x0, l_.str@PAGE
+	add	x0, x0, l_.str@PAGEOFF
+	bl	_printf
+	ldr	w0, [sp, #12]                   ; 4-byte Folded Reload
+	ldp	x29, x30, [sp, #48]             ; 16-byte Folded Reload
+	add	sp, sp, #64
+	ret
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"Elapsed: %.20lf\n"
+
 .subsections_via_symbols
