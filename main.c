a3ae8c15af2ef0945162d8ec72fbd65ab844fdef
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-19 20:08:20 +0100

    Test buyers allocation: success

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 
 #include <stdio.h>
+#include "err.h"
 #include "buyer.h"
 #include "order.h"
 
 int main(int argc, char *argv[]){
+	err_t err;
+
+	buyer8_t *buyers = NULL;
+	err = buyer_alloc(&buyers, 100);
+	if( err == BAD_ALLOC){
+		fprintf(stderr, "Bad alloc for buyers\n");
+		return 1;
+	}
+
+	printf("&buyers = %p\tbuyers = %p\tbuyers->id = %llu\n", &buyers, buyers, buyers->id);
+
+	/*
+	 * Everything fine, release memory
+	 */
+	buyer_free(&buyers);
 
 	return 0;
 }
--- main.s
+++ main.s
@@ -5,8 +5,67 @@
 _main:                                  ; @main
 	.cfi_startproc
 ; %bb.0:
-	mov	w0, #0
+	sub	sp, sp, #64
+	stp	x20, x19, [sp, #32]             ; 16-byte Folded Spill
+	stp	x29, x30, [sp, #48]             ; 16-byte Folded Spill
+	add	x29, sp, #48
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+	.cfi_offset w19, -24
+	.cfi_offset w20, -32
+	str	xzr, [sp, #24]
+	add	x19, sp, #24
+	add	x0, sp, #24
+	mov	w1, #100
+	bl	_buyer_alloc
+	cmp	w0, #128
+	b.ne	LBB0_2
+; %bb.1:
+Lloh0:
+	adrp	x8, ___stderrp@GOTPAGE
+Lloh1:
+	ldr	x8, [x8, ___stderrp@GOTPAGEOFF]
+Lloh2:
+	ldr	x3, [x8]
+Lloh3:
+	adrp	x0, l_.str@PAGE
+Lloh4:
+	add	x0, x0, l_.str@PAGEOFF
+	mov	w19, #1
+	mov	w1, #21
+	mov	w2, #1
+	bl	_fwrite
+	b	LBB0_3
+LBB0_2:
+	ldr	x8, [sp, #24]
+	ldr	x9, [x8]
+	stp	x8, x9, [sp, #8]
+	str	x19, [sp]
+Lloh5:
+	adrp	x0, l_.str.1@PAGE
+Lloh6:
+	add	x0, x0, l_.str.1@PAGEOFF
+	bl	_printf
+	add	x0, sp, #24
+	bl	_buyer_free
+	mov	w19, #0
+LBB0_3:
+	mov	x0, x19
+	ldp	x29, x30, [sp, #48]             ; 16-byte Folded Reload
+	ldp	x20, x19, [sp, #32]             ; 16-byte Folded Reload
+	add	sp, sp, #64
 	ret
+	.loh AdrpAdd	Lloh3, Lloh4
+	.loh AdrpLdrGotLdr	Lloh0, Lloh1, Lloh2
+	.loh AdrpAdd	Lloh5, Lloh6
 	.cfi_endproc
                                         ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_.str:                                 ; @.str
+	.asciz	"Bad alloc for buyers\n"
+
+l_.str.1:                               ; @.str.1
+	.asciz	"&buyers = %p\tbuyers = %p\tbuyers->id = %llu\n"
+
 .subsections_via_symbols
