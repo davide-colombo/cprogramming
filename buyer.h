c467e30bd8845856ea829afb70cfda753d446f5d
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-19 17:04:03 +0100

    Introducing err.h

--- buyer.h
+++ buyer.h
@@ -2,14 +2,39 @@
 #ifndef BUYER_H
 #define BUYER_H
 
+#include <stdio.h>
 #include <stdint.h>
+#include "err.h"
 
 #define MAX_BUYERS 1024
 
 // 8-byte size and alignment
-typedef struct _buyer {
+typedef struct buyer8 {
 	uint64_t id;
-} buyer;
+} buyer8_t;
+
+/*
+ * Interface to dynamically allocate memory for "n" objects of type 
+ * "buyer8_t".
+ *
+ * If memory allocation fails "BAD_ALLOC" error code is returned and the value 
+ * pointed to by `bptr` is not touched.
+ *
+ * If memory allocation succeeds "SUCC_ALLOC" code is returned and the value 
+ * pointed to by `bptr` is the starting memory address of the dynamically 
+ * allocated memory.
+ *
+ * The caller MUST REMEMBER TO CALL the opposite function `buyer_free()` to 
+ * release memory as soon as it is no longer needed.
+ *
+ */
+err_t buyer_alloc(buyer8_t **bptr, size_t n);
+
+/*
+ * Release previously dynamically allocated memory pointed to by "bptr" 
+ * pointer.
+ */
+err_t buyer_free(buyer8_t *bptr);
 
 
 #endif	/* BUYER_H */
---
+++ err.h
@@ -0,0 +1,33 @@
+/*
+ * Define error and success status code and data types.
+ */
+#ifndef ERR_H
+#define ERR_H
+
+#include <stdint.h>
+
+/*
+ * Error data type.
+ *
+ * Every function in the project should return a value of this type.
+ *
+ * The possible values are defined below by the #define directive.
+ *
+ * There is room for 256 integer code from 0 to 255.
+ *
+ * The code space is divided into two using the most significant bit:
+ *
+ * 0, success code
+ * 1, error code
+ *
+ * This may be changed in future extension without problems since the #define 
+ * directive provide an abstraction layer to hide the implementation details 
+ * of the error code convention.
+ */
+typedef uint8_t err_t;
+
+
+#define SUCC_ALLOC		(err_t) 0x00
+#define BAD_ALLOC		(err_t) 0x80
+
+#endif	/* ERR_H */
