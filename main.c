67a3f9500de2adcb1394a22c90b6149eb8ef2969
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-20 10:49:23 +0100

    Added main.c for testing performance of particles

---
+++ main.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <time.h>
+#include "particles.h"
+
+/*
+ * Allocate a static array of particles
+ */
+particle3_t particles[PARTICLES];
+
+int main(int argc, char *argv[]){
+	float mass = 457.987;
+	clock_t start, end;
+	start = clock();
+	particle3_gravitational_force(&particles[0], mass);
+	end = clock();
+	double elapsed = end - start;
+	double icps = 1.0f / (double) CLOCKS_PER_SEC;
+	double res = elapsed * icps;
+	printf("Elapsed = %.20f\n", res);
+	return 0;
+}
