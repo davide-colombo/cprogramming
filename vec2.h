c2d090a4f1cf9cca54d9bb48fcfd15706f1f8152
Davide Colombo <davide.colombo02@protonmail.com>
2022-12-20 16:09:44 +0100

    A first definition of vec2 interface

---
+++ vec2.h
@@ -0,0 +1,29 @@
+
+#ifndef MATRIX_H_INCLUDED
+#define MATRIX_H_INCLUDED
+
+#define NROWS 1000
+#define NCOLS 1000
+
+/*
+ * Numeric data type
+ */
+typedef double number_t;
+
+/*
+ * Two dimensional vector represented as a 2D array of items
+ */
+typedef (*number_t)[NROWS][NCOLS] vector2_t;
+
+
+/*
+ * Print the content of the 2d vector item
+ */
+void vector2_print(vector2_t *vec2);
+
+/*
+ * Randomly initialize the 2d vector
+ */
+void vector2_rand_init(vector2_t *vec2, double scale, double shift);
+
+#endif
